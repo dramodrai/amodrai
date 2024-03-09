1d07a8064df40716483ea9c7f353608fe42ca232
dramodrai <dr.amodrai@gmail.com>
2024-03-09 22:45:38 +0530

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import alpine from "@astrojs/alpinejs";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import { SITE } from "./src/config";
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: SITE.website,
+  integrations: [tailwind(), alpine(), sitemap()],
+});
